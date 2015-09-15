346285658822cc96e24b23098150c699aa355c9c
Anthony <tony0000.ac@gmail.com>
2015-09-15 20:20:36 +0200

    Create Main.php

---
+++ src/SmallPvPUtils/Main.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace SmallPvPUtils;
+
+use pocketmine\plugin\PluginBase;
+use pocketmine\event\Listener;
+use pocketmine\event\player\PlayerJoinEvent;
+use pocketmine\event\player\PlayerDeathEvent;
+use pocketmine\event\player\PlayerQuitEvent;
+use pocketmine\Player;
+
+class Main extends PluginBase implements Listener
+
+{
+
+  public function onEnable(){
+    $this->getLogger()->info(TextFormat::GREEN."Enabled!");
+    $this->getServer()->getPluginManager()->registerEvents($this, $this);
+  }
+
+  public function onDisable(){
+    $this->getLogger()->info(TextFormat::RED."Disabled!");
+  }
+
+  public function onPlayerJoin(PlayerJoinEvent $event){
+    $p = $event->getPlayer();
+    if($p instanceof Player){
+      $p->getInventory()->clearAll();
+    }
+  }
+
+  public function onPlayerDeath(PlayerDeathEvent $event){
+    $p = $event->getPlayer();
+    if($p instanceof Player){
+      $p->getInventory()->clearAll();
+    }
+  }
+
+  public function onPlayerQuit(PlayerQuitEvent $event){
+    $p = $event->getPlayer();
+    if($p instanceof Player){
+      $p->getInventory()->clearAll();
+    }
+  }
+
+}
